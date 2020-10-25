ceee4ee21df43e562a3ce132908c8c144d325d10
Intisar Mahee <67532512+intisar-mahee@users.noreply.github.com>
2020-10-25 14:33:20 +0600

    finalTOunch

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -4,13 +4,28 @@ const Cart = (props) => {
     const cart=props.cart;
     const total= cart.reduce((total, prd)=> total+prd.price, 0)
     // const totalPrice = cart.reduce((total, prd)=> total + prd.price, 0);
-
+let shipping =0;
+if(total>15){
+    shipping=0;
+}
+else if(total>35){
+    shipping=0;
+}
+else if(total>0){
+    shipping=12.69
+}
+const tax= Math.round(total/10);
+const grandTotal= Math.round(total+tax+shipping);
 
     return (
         <div>
             <h4>Order Summary</h4>
-    <p>Items Ordered: {cart.length}</p>
-    <p>Total Price:{total}</p>
+            <p>Items Ordered: {cart.length}</p>
+            <p>Product Price {total}</p>
+            <p>Shipping Cost: {shipping}</p>
+            <p>Tax: {tax}</p>
+            <p>Total Price: {grandTotal}</p>
+    
         </div>
     );
 };
