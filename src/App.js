dbdc881966ee5bf40382c7115a7a76b760ef2e58
Intisar Mahee <67532512+intisar-mahee@users.noreply.github.com>
2020-11-15 14:10:24 +0600

    update1

--- src/App.js
+++ src/App.js
@@ -3,12 +3,39 @@ import './App.css';
 import Header from './components/Header/Header';
 import Product from './components/Product/Product';
 import Shop from './components/Shop/Shop';
+import Review from './components/Review/Review'
+import Inventory from './components/Inventory/Inventory'
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Notfound from './components/Notfound/Notfound';
 
 function App() {
   return (
     <div>
-        <Header></Header>
-        <Shop></Shop>
+      <Header></Header>
+      <Router>
+         <Switch>
+            <Route exact path="/Shop">
+              <Shop></Shop>
+            </Route>
+            <Route path="/Review">
+              <Review></Review>
+            </Route>
+            <Route path='/manage'>
+            <Inventory></Inventory>
+            </Route>
+            <Route path='*'>
+                          <Notfound></Notfound>
+            </Route>
+         </Switch>
+      </Router>
+        
+        
         
     </div>
   );
--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -2,11 +2,12 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 import './Product.css';
+import { Link } from 'react-router-dom';
 
 
 const Product = (props) => {
-    // console.log(props);
-    const { img, name, seller, price, stock } = props.product;
+    console.log(props.product.key);
+    const { img, name, seller, price, stock, key } = props.product;
     return (
         <div className="product">
             <div >
@@ -14,7 +15,7 @@ const Product = (props) => {
             </div>
 
             <div>
-            <h4 className="product-name">{name}</h4>
+            <h4 className="product-name"><Link to={"/"+key}>{name}</Link></h4>
             <br />
             <p><small>by: {seller}</small></p>
             <p>${price}</p>
